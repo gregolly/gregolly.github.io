563634fea4431215cc4946a75bb418b31bad6ec9
Gregolly <gregollyff@hotmail.com>
2020-04-17 00:30:19 -0400

    Meu portfolio

---
+++ js/slider.js
@@ -0,0 +1,14 @@
+function initSlider(){
+    $('.slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        autoplay: false,
+    });
+}
+
+// $(document).ready(function(){
+//     initSlider();
+// });
+    
