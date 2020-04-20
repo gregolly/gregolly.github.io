ba26b794b2f8150acc91bb85bc2dcfea83571dce
Gregolly <gregollyff@hotmail.com>
2020-04-19 21:02:18 -0400

    Slide nos projetos

--- js/slider.js
+++ js/slider.js
@@ -5,6 +5,9 @@ function initSlider(){
         speed: 300,
         slidesToShow: 1,
         autoplay: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
     });
 }
 
